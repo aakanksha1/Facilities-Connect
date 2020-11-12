20b866c5ec77e289473e5e8c66fd276639b74ec4
aakanksha1 <aakanksha720@gmail.com>
2020-11-12 12:51:15 -0600

    pandas

--- project/hello_world.py
+++ project/hello_world.py
@@ -7,6 +7,42 @@ Created by Guillaume Chevalier at Neuraxio:
 License: CC0-1.0 (Public Domain)
 """
 
+#https://www.datacamp.com/community/tutorials/python-excel-tutorial
+
+# READ IN EXCEL FILES USING PANDAS
+# FOR ROW IN BUILDING_CHARGED, ITERATE OVER JOB_ID
+	# IF BLANK, SKIP
+	# ELSE GRAB ROW INFORMATION
+		#INITIALIZE [] = PROCEDURE_NUMBER
+		#INITIALIZE [] = 
+		#ITERATE THROUGH PROCEDURES_TRANSFER FILTER BY PROCEDURE_TITLE
+		#FILL ARRAY WITH PROCEDURE_NUMBERS
+
+
+
+# the run time for this 
+
+# Import pandas
+import pandas as pd
+
+# Assign spreadsheet filename to `file`
+file = 'example.xlsx'
+
+# Load spreadsheet
+xl = pd.ExcelFile(file)
+
+# Print the sheet names
+print(xl.sheet_names)
+
+# Load a sheet into a DataFrame by name: df1
+df1 = xl.parse('Sheet1')
+
+
+
+
+
 def hello_world():
-    return "Hello World!"
+	print "Hello World!"
+    
+
 
